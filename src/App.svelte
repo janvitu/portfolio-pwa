6d6d240089957f3a7b0fda44b9b389f79e26c8e4
Jan Vitu <jan@janvitu.dev>
2021-03-08 20:25:15 +0100

    💃 Animation

--- src/App.svelte
+++ src/App.svelte
@@ -3,6 +3,19 @@
   import router from "@/router/index.js";
 
   import { routeCurrent } from "@/store/routeState.js";
+  import { onMount } from "svelte";
+
+  import { animationObserver } from "@js/use/siteAnimation";
+
+  onMount(() => {
+    if ("IntersectionObserver" in window) {
+      const animationElements = document.querySelectorAll(`[data-animate]`);
+      animationElements.forEach((animationElement) => {
+        animationElement.classList.add("opacity-0");
+        animationObserver.observe(animationElement);
+      });
+    }
+  });
 
   router();
 </script>
